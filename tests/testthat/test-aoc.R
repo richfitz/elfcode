1ac583e1f9e6f1d247b3c3a8bac7aa18e010db6e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-12-21 12:41:16 +0000

    Add day 19 tuned version

--- tests/testthat/test-aoc.R
+++ tests/testthat/test-aoc.R
@@ -13,3 +13,11 @@ test_that("day21", {
   res <- elfcode_run(p, r = r1, max = 1849)
   expect_identical(res, list(c(11285115L, 11285115L, 30L, 1L, 1L, 1L), 1848L))
 })
+
+
+test_that("day19 - tuned", {
+  p <- elfcode_compile(file = "tuned19.txt")
+  r <- c(0, 0, 0, 0, 0, 939)
+  res <- elfcode_run(p, r = r, max = 18801)
+  expect_identical(res, list(c(1256L, 1L, 31L, 961L, 99L, 939L), 18800L))
+})
