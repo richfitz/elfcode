cc490fc0451314f1ce9957a43c3af309ab655a18
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-12-21 10:46:33 +0000

    Add integration tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(elfcode)
+
+test_check("elfcode")
---
+++ tests/testthat/test-aoc.R
@@ -0,0 +1,15 @@
+context("advent of code")
+
+test_that("day19", {
+  p <- elfcode_compile(file = "input19.txt")
+  res <- elfcode_run(p, max = 7057537)
+  expect_identical(res, list(c(1256L, 1L, 940L, 940L, 256L, 939L), 7057536L))
+})
+
+
+test_that("day21", {
+  p <- elfcode_compile(file = "input21.txt")
+  r1 <- c(11285115, rep(0, 5))
+  res <- elfcode_run(p, r = r1, max = 1849)
+  expect_identical(res, list(c(11285115L, 11285115L, 30L, 1L, 1L, 1L), 1848L))
+})
